4676f2289ec4171704cd8436584423239aeb13dd
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2018-02-19 06:42:28 -0800

    Initial Commit

---
+++ VirtualTourist_9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist_9
+//
+//  Created by Michelle Grover on 2/19/18.
+//  Copyright © 2018 Norbert Grover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
